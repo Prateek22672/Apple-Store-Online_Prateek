5d32ec5bf35e151cfcd77606bb9409247db4f46d
Prateek™ <prateek.koratala@gmail.com>
2024-10-01 19:18:49 +0530

    Add files via upload

---
+++ non-guest_index.js
@@ -0,0 +1,30 @@
+function validateForm(event) {
+    event.preventDefault(); // Prevent the default form submission
+    
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('pass').value;
+    const errorMessage = document.getElementById('error-message');
+
+    if (password === "OG") {
+        // Store email in local storage
+        localStorage.setItem('email', email);
+        
+        // If both fields are filled correctly, redirect to the next page
+        window.location.href = 'store.html';
+    } else { 
+        // Display error message if email or password is incorrect
+        errorMessage.style.display = 'block';
+    }
+}
+
+// Add event listener for Enter key press
+document.getElementById('email').addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+        validateForm(event);
+    }
+});
+document.getElementById('pass').addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+        validateForm(event);
+    }
+});
