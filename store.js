9d4cf9e3f107caab914c2ba17db6197d438a23a7
Prateek™ <prateek.koratala@gmail.com>
2024-10-02 11:53:06 +0530

    Update store.js

--- store.js
+++ store.js
@@ -1,3 +1,33 @@
+// Function to update the display of cart quantity
+function updateCartQuantityDisplay() {
+    const cartItems = JSON.parse(localStorage.getItem('cart')) || [];//This line fetches the cart from local storage and parses it into a JavaScript array. If no cart data is found, it defaults to an empty array ([]).
+    const totalQuantity = cartItems.reduce((total, item) => total + item.quantity, 0);//it uses the reduce function to sum up the quantity of all items in the cart.
+    const quantElement = document.getElementById('quant');
+
+    if (quantElement) {
+        quantElement.innerText = totalQuantity;
+
+        if (totalQuantity > 0) {
+            quantElement.style.display = 'block';
+        } else {
+            quantElement.style.display = 'none';
+        }
+
+        if (totalQuantity > 9) {
+            quantElement.style.paddingRight = '18px';
+        } else {
+            quantElement.style.paddingRight = '0';
+        }
+    }
+}
+
+// Event listener to update cart quantity display on page load
+document.addEventListener('DOMContentLoaded', updateCartQuantityDisplay);
+
+
+
+
+
 const splashScreen = document.getElementById('splash-screen');
 
 // Always show the splash screen on page load
@@ -10,6 +40,8 @@ setTimeout(() => {
         splashScreen.style.display = 'none'; // Hide splash screen
     }, 1000); // Duration for fade-out transition
 }, 1600); // Duration to show splash screen
+
+
 
 
  
@@ -37,4 +69,5 @@ if (name) {
 } else if (email) {
     greetingElement.innerHTML = `Hello ${email},<br>`;
 }
+
 
